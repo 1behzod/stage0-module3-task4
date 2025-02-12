85194777e5795d8825c4af2ca3c650c5705153e0
Behzod <behzodjaloliddinov870@gmail.com>
2025-02-12 15:42:29 +0500

    aq

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int[] asciiValues = {99, 104, 97, 114};
+
+        for (int value : asciiValues) {
+            System.out.print((char) value);
+        }
     }
 }
